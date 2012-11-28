70773333b93c82b4fd4d8147e4689474f7c79b3e
Jacob Fischer <jtf3m8@mst.edu>
2012-11-28 01:01:15 -0600

    More API Test
    Added a service to be tested, there is one test to make sure it gets added

--- whiskerboard/tests/api.py
+++ whiskerboard/tests/api.py
@@ -8,6 +8,10 @@ class ServiceAPITestCase(unittest.TestCase):
     def setUp(self):
         self.client = Client()
 
+        a = Service.objects.create(service_name='A Service', description='A service description.')
+        #b = Service.objects.create(service_name='B Service', description='B service description.')
+        self.services = [a]
+
     def test_root_get(self):
         # HTTP 200 for GET /api/v1/services
         r = self.client.get('/api/v1/services')
@@ -27,4 +31,11 @@ class ServiceAPITestCase(unittest.TestCase):
         # HTTP 405 for PUT /api/v1/services
         r = self.client.put('/api/v1/services')
         self.assertEqual(r.status_code, 405, 'PUT service endpoint returns HTTP 405')
-        
+
+
+
+    def test_service_added(self):
+        r = self.client.get('/api/v1/services')
+        rJson = json.loads(r.content)
+        # should probably get the string from the self.services var
+        self.assertEquals("A Service", rJson["services"][0]["name"])
