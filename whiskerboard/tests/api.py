ccebf25365dbf227ddb6b8b641d3293d1e932f20
Jacob Fischer <jtf3m8@mst.edu>
2012-11-28 00:29:21 -0600

    More API tests
    Added a few more basic tests for the API response codes

--- whiskerboard/tests/api.py
+++ whiskerboard/tests/api.py
@@ -12,4 +12,19 @@ class ServiceAPITestCase(unittest.TestCase):
         # HTTP 200 for GET /api/v1/services
         r = self.client.get('/api/v1/services')
         self.assertEqual(r.status_code, 200, 'GET service endpoint returns HTTP 200')
+
+    def test_root_post(self):
+        # HTTP 405 for POST /api/v1/services
+        r = self.client.post('/api/v1/services')
+        self.assertEqual(r.status_code, 405, 'POST service endpoint returns HTTP 405')
+
+    def test_root_delete(self):
+        # HTTP 405 for DELETE /api/v1/services
+        r = self.client.delete('/api/v1/services')
+        self.assertEqual(r.status_code, 405, 'DELETE service endpoint returns HTTP 405')
+
+    def test_root_put(self):
+        # HTTP 405 for PUT /api/v1/services
+        r = self.client.put('/api/v1/services')
+        self.assertEqual(r.status_code, 405, 'PUT service endpoint returns HTTP 405')
         
